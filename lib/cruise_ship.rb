26203132a1ec3e01da5ea259f9cf009ab76739d5
Aiden Marques <aiden.marques94@gmail.com>
2020-09-21 04:51:53 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,16 +1,15 @@
 # passengers = {
-#suite_a: "Amanda Presley", 
+#uite_a: "Amanda Presley", 
 # suite_b: "Seymour Hoffman", 
 # suite_c: "Alfred Tennyson", 
 # suite_d: "Charlie Chaplin", 
 # suite_e: "Crumpet the Elf"
 # }
 
-winner = ""
-passengers.each do |suite, name|
+def select_winner(passengers)
+  winner = ""
+  passengers.each do |suite, name| 
   if suite == :suite_a && name.start_with?("A")
     winner = name
   end
 end
- 
-winner
