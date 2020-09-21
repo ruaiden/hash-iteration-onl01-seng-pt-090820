b5ebd90804143c4ecb64e38c0367d8f9185be4e5
Aiden Marques <aiden.marques94@gmail.com>
2020-09-21 04:56:00 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,7 +5,10 @@
 # }
 
 def happy_birthday(birthday_kids)
-        birthday_kids.each do |kids_name, age| puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+        birthday_kids.each do |kids_name, age| 
+          if age > 12 
+        puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+      end
       end
 end
 
