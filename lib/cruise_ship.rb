5f06b4c0b1b5057879d07a036420fd792a3c1774
Aiden Marques <aiden.marques94@gmail.com>
2020-09-21 04:46:27 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -9,7 +9,7 @@ ss# passengers = {
 def select_winner(passengers)
   passengers.each do |suit, name| 
     
-  if suit == :suite_a && name.start_with? ("A")
+  if suite == :suite_a && name.start_with? ("A")
     winner = name
   end
 end
