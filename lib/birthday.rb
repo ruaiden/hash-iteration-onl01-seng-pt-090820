9d8d8ec35c4bb83223f71760c4463278f36ed05d
Aiden Marques <aiden.marques94@gmail.com>
2020-09-21 04:54:51 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,7 +5,8 @@
 # }
 
 def happy_birthday(birthday_kids)
-        birthday_kids do |kids_name, age| puts ""Happy Birthday #{kids_name}! You are now #{age} years old!""
+        birthday_kids do |kids_name, age| puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+      end
 end
 
 
